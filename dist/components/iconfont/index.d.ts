06fbe8d269be177bad3a8742cbab2be9592e35fc
红豆饼 <1254704208@qq.com>
2023-04-19 21:34:30 +0800

    评论审核页面

---
+++ dist/components/iconfont/index.d.ts
@@ -0,0 +1,13 @@
+/* eslint-disable */
+import React, { FunctionComponent } from 'react';
+
+interface Props {
+  name: 'chazhao' | 'tishi1' | 'xihuan' | 'a-1' | 'a-11' | 'biaoqing' | 'fanhui' | 'dingwei' | 'cha' | 'gengduo' | 'bofang' | 'fenxiang' | 'gou1' | 'jiahao' | 'fangda' | 'jiantouxiangxia' | 'jinggao' | 'quancha' | 'quanxiangyou' | 'quanxiangzuo' | 'quangou' | 'saoma' | 'shanchu' | 'shezhi' | 'shangchuan' | 'tupian' | 'tishi' | 'touxiang1' | 'wenjianjiaguan' | 'shuaxin' | 'zan' | 'youjian' | 'zhuce2' | 'zhuye' | 'gengduo1' | 'fenxiang1' | 'qian' | 'xin' | 'wujiaoxing' | 'shoucang' | 'cog-fill' | 'times-circle-fill' | 'check-circle-fill' | 'undo' | 'redo' | 'loading' | 'scan' | 'gift' | 'link' | 'poweroff' | 'laptop' | 'nail-fixed' | 'edit' | 'eyeslash-fill' | 'position-fill' | 'copy-fill' | 'pausecircle-fill' | 'playcircle-fill' | 'heart-fill' | 'collection-fill';
+  size?: number;
+  color?: string | string[];
+  style?: React.CSSProperties;
+}
+
+declare const IconFont: FunctionComponent<Props>;
+
+export default IconFont;
