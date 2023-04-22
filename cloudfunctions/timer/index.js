c0413279475a5adf2ae3698550832799818dd996
wangerke <3250003070@qq.com>
2023-04-22 21:59:38 +0800

    优化云函数触发器性能，用Promise.all()替代低效的for循环await

--- cloudfunctions/timer/index.js
+++ cloudfunctions/timer/index.js
@@ -8,18 +8,22 @@ exports.main = async () => {
     var res = ''
     const collect = (await db.collection('hotel').orderBy('lastCntTime', "asc").get()).data
     let index = Math.floor(Math.random() * (collect.length - 5))
-    const docs = collect.slice(index, index + 10)
-    for (let doc of docs) {
+    const docs = collect.slice(index, index + 20)
+    const promises = docs.map(async (doc) => {
         let hid = doc._id
         const commentCnt = await db.collection('comment').where({ hotelId: hid, pass: true }).count()
-        res += hid + ":" + commentCnt.total + "; "
         db.collection('hotel').doc(hid).update({
             data: {
                 lastCntTime: new Date().valueOf(),
                 commentNum: commentCnt.total
             }
         })
-    }
+        return hid + ":" + commentCnt.total + "; "
+    })
+    const results = await Promise.all(promises)//用Promise.all替代了原本低效的for循环await
+    results.forEach((result) => {
+        res += result
+    })
     return res
 
 }
